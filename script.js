a154c53c4b32dfb923e8fec410a3d9e467516d75
JACINTO WONG <jacinto.wong@cbc.ca>
2020-04-16 14:50:13 -0400

    final production code

--- script.js
+++ script.js
@@ -1,68 +1,74 @@
-const quoteEl = document.getElementById("quote");
-const authorEl = document.getElementById("author");
-const twitterBtn = document.getElementById("twitter");
-const newQuoteBtn = document.getElementById("new-quote");
-const loader = document.getElementById("loader");
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
 // Loading Spinner Shown
 function loading() {
-  quoteEl.style.visibility = "hidden";
-  authorEl.style.visibility = "hidden";
-  loader.style.visibility = "visible";
+  loader.hidden = false;
+  quoteText.hidden = true;
+  quoteContainer.hidden = true;
+  authorText.hidden = true;
 }
 
 // Remove Loading Spinner
 function complete() {
-  if (loader.style.visibility === "visible") {
-    quoteEl.style.visibility = "visible";
-    authorEl.style.visibility = "visible";
-    loader.style.visibility = "hidden";
+  if (!loader.hidden) {
+    quoteContainer.hidden = false;
+    quoteText.hidden = false;
+    authorText.hidden = false;
+    loader.hidden = true;
   }
 }
 
 // Get Quote From API
-newQuoteBtn.addEventListener("click", getQuote);
 function getQuote() {
   loading();
-  const proxyUrl = "https://cors-anywhere.herokuapp.com/";
-  const apiUrl =
-    "https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json";
+  // We need to use a Proxy URL to make our API call in order to avoid a CORS error
+  const proxyUrl = 'https://jacinto-cors-proxy.herokuapp.com/';
+  const apiUrl = 'https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
   fetch(proxyUrl + apiUrl)
     .then((res) => res.json())
     .then((data) => {
-      console.log(data);
       // Check if Author field is blank and replace it with 'Unknown'
-      if (data.quoteAuthor === "") {
-        authorEl.innerHTML = "Unknown";
+      if (data.quoteAuthor === '') {
+        authorText.innerText = 'Unknown';
       } else {
-        authorEl.innerHTML = data.quoteAuthor;
+        authorText.innerText = data.quoteAuthor;
       }
       // Dynamically reduce font size for long quotes
-      console.log(data.quoteText.length);
       if (data.quoteText.length > 120) {
-        quoteEl.classList.add("long-quote");
+        quoteText.classList.add('long-quote');
       } else {
-        quoteEl.classList.remove("long-quote");
+        quoteText.classList.remove('long-quote');
       }
-      quoteEl.innerHTML = data.quoteText;
+      quoteText.innerText = data.quoteText;
     })
-    .catch((err) => {
-      getQuote();
-      //console.log(err);
+    .catch(() => {
+      // Can be used to log errors, etc. 
     });
-  if (authorEl.innerHTML !== undefined) {
+
+  // Check to see if quote has loaded
+  if (authorText.innerText !== undefined) {
+    // Timeout to give the loader a chance to spin at least half a second, 
+    // because some quotes take longer to load
     setTimeout(complete, 500);
   }
 }
 
 // Tweet Quote
-twitterBtn.addEventListener("click", tweetQuote);
 function tweetQuote() {
-  let quote = quoteEl.innerHTML;
-  let author = authorEl.innerHTML;
-  let twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
-  window.open(twitterUrl, "_blank");
+  const quote = quoteText.innerText;
+  const author = authorText.innerText;
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+  window.open(twitterUrl, '_blank');
 }
+
+// Event Handlers
+newQuoteBtn.addEventListener('click', getQuote);
+twitterBtn.addEventListener('click', tweetQuote);
 
 // On Load
 getQuote();
