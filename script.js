4c32a4f65dc137e9de9df594726779d90fe3d40a
JACINTO WONG <jacinto.wong@cbc.ca>
2020-07-19 14:20:58 -0400

    final production code

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ function complete() {
 async function getQuote() {
   loading();
   // We need to use a Proxy URL to make our API call in order to avoid a CORS error
-  const proxyUrl = 'https://jacinto-cors-proxy.herokuapp.com/';
+  const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
   const apiUrl = 'https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
   try {
     const response = await fetch(proxyUrl + apiUrl);
