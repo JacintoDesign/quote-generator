ac566393e4fc7d4406c2329e1632efb3992de780
JACINTO WONG <jacinto.wong@cbc.ca>
2020-09-02 22:44:14 -0400

    final production code

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
 const loader = document.getElementById('loader');
 
+let allQuotes = [];
+
 // Loading Spinner Shown
 function loading() {
   loader.hidden = false;
@@ -19,30 +21,39 @@ function complete() {
   }
 }
 
-// Get Quote From API
-async function getQuote() {
+// Get Quotes From API
+async function getQuotes() {
   loading();
   const apiUrl = 'https://type.fit/api/quotes';
   try {
     const response = await fetch(apiUrl);
-    const allQuotes = await response.json();
-    // Pick a random quote from array
-    const quote = allQuotes[Math.floor(Math.random() * allQuotes.length)];
-    // Set Author
-    authorText.textContent = quote.author;
-    // Check Quote length to determine styling
-    if (quote.text.length > 120) {
-      quoteText.classList.add('long-quote');
-    } else {
-      quoteText.classList.remove('long-quote');
-    }
-    // Set Quote
-    quoteText.textContent = quote.text;
-    // Hide Loader
-    complete();
+    allQuotes = await response.json();
+    newQuote();
   } catch (error) {
     // Catch Error Here
   }
+}
+
+// Show New Quote
+function newQuote() {
+  loading();
+  // Pick a random quote from array
+  const quote = allQuotes[Math.floor(Math.random() * allQuotes.length)];
+  // Check if Author field is blank and replace it with 'Unknown'
+  if (!quote.author) {
+    authorText.textContent = 'Unknown';
+  } else {
+    authorText.textContent = quote.author;
+  }
+  // Check Quote length to determine styling
+  if (quote.text.length > 120) {
+    quoteText.classList.add('long-quote');
+  } else {
+    quoteText.classList.remove('long-quote');
+  }
+  // Set Quote, Hide Loader
+  quoteText.textContent = quote.text;
+  complete();
 }
 
 // Tweet Quote
@@ -54,8 +65,8 @@ function tweetQuote() {
 }
 
 // Event Listeners
-newQuoteBtn.addEventListener('click', getQuote);
+newQuoteBtn.addEventListener('click', newQuote);
 twitterBtn.addEventListener('click', tweetQuote);
 
 // On Load
-getQuote();
+getQuotes();
