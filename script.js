c991d764e03e4d1719a8ba3d70c63f4c5c6cf55d
Jacinto Wong <jacinto.wong@cbc.ca>
2022-08-23 09:10:33 -0400

    updated to use my own Quotes API

--- script.js
+++ script.js
@@ -44,7 +44,7 @@ function newQuote() {
 // Get Quotes From API
 async function getQuotes() {
   loading();
-  const apiUrl = 'https://type.fit/api/quotes';
+  const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
   try {
     const response = await fetch(apiUrl);
     apiQuotes = await response.json();
